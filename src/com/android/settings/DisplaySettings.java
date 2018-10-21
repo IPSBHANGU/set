aded41f3feac131245f46793924160f8114c30d4
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-10-21 07:34:25 -0400

    settings: Themes: Add Black variant to styles [3/3]
    CarbonROM edit: Use default SwitchPreference with a PreferenceController
    
    Change-Id: Ifd1a175f5d81b0d39dc25c7b0a03483f7a46b264
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -25,6 +25,7 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AccentPickerPreferenceController;
 import com.android.settings.display.AmbientDisplayPreferenceController;
+import com.android.settings.display.BlackThemePreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.ColorModePreferenceController;
@@ -97,6 +98,7 @@ public class DisplaySettings extends DashboardFragment {
                 new AmbientDisplayConfiguration(context),
                 KEY_AMBIENT_DISPLAY));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new BlackThemePreferenceController(context));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
