Srayons/android_packages_apps_Settings

StorageSettings: Use pixel color in storage bar / icon

src/com/android/settings/deviceinfo/StorageSettings.java
@@ -78,7 +78,7 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
     static final int COLOR_PUBLIC = Color.parseColor("#ff9e9e9e");
 
     static final int[] COLOR_PRIVATE = new int[] {
-            Color.parseColor("#ff26a69a"),
+            Color.parseColor("#ff4285f4"),
             Color.parseColor("#ffab47bc"),
             Color.parseColor("#fff2a600"),
             Color.parseColor("#ffec407a"),

IPSBHANGU/set

Change-Id: Id56ea292117df7abdb7a53cff76bc0328977f4f1
Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

src/com/android/settings/deviceinfo/StorageSettings.java
@@ -77,7 +77,7 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
     static final int COLOR_PUBLIC = Color.parseColor("#ff9e9e9e");
 
     static final int[] COLOR_PRIVATE = new int[]{
-            Color.parseColor("#ff26a69a"),
+            Color.parseColor("#ff1a73e8"),
             Color.parseColor("#ffab47bc"),
             Color.parseColor("#fff2a600"),
             Color.parseColor("#ffec407a"),